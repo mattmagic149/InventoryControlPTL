3e31393115275c35cca569b4a79ce56039f59233
Matthias Ivantsits <matthias.ivantsits@student.tugraz.at>
2015-09-11 15:16:29 +0200

    added scanning servlet

---
+++ src/servlet/Scanning.java
@@ -0,0 +1,51 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Welcome
+ */
+@WebServlet("/Scanning")
+public class Scanning extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Scanning() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		
+		/*if(session.getAttribute("currentUser") == null) {
+			request.getRequestDispatcher("index.jsp").include(request, response);
+			System.out.println("NOT logged in");
+			return;
+		}	*/
+		
+		System.out.println("Welcome has been called...");
+		request.getRequestDispatcher("scanning.jsp").include(request, response);
+		return;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
