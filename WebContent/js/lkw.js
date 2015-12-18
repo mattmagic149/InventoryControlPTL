d29a40c78f836300067fc0cc43603c3e9aab6244
Michael Scheucher <michischeucher@gmail.com>
2015-12-18 11:30:22 +0100

    added ui for showing transaction

--- WebContent/js/lkw.js
+++ WebContent/js/lkw.js
@@ -34,7 +34,7 @@ function handleClickOnShowServices(e) {
 
 function handleClickOnShowProducts(e) {
 	var id = $("#show_products").attr("truck_id");
-	location.href = "ProductsOverview?location_id=" + id;
+	location.href = "TransactionsOverview?location_id=" + id;
 }
 
 function handleChangeBrand(e) {
